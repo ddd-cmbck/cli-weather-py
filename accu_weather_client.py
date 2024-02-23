f950c49086cf3dfa354abeacdd7eaaa588307325
Dim4k <dd.darknet.tt@gmail.com>
2024-02-23 23:15:50 +0000

    initialized class for accuWeather api and tests for it

---
+++ accu_weather_client.py
@@ -0,0 +1,10 @@
+class AccuWeatherClient:  # to do
+    """
+
+    Fetch weather data from https://www.accuweather.com/
+    and convert the API response into WeatherData format
+
+    """
+
+    def __init__(self):
+        pass
---
+++ test_accu_weather_client.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import patch
+
+from accu_weather_client import AccuWeatherClient
+
+
+class TestAccuWeatherClient(unittest.TestCase):  # to do
+
+    def setUp(self):
+        accu_cl = AccuWeatherClient()
+
+    def test_get_weather(self):
+        pass
+

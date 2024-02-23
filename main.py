7f4a8225ae2627d89b7dda530c56ed505a714439
Dim4k <dd.darknet.tt@gmail.com>
2024-02-23 22:40:04 +0000

    new architecture considered to be

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-import datetime as dt
-import requests
 
-BASE_URL = 'https://dataservice.accuweather.com/forecasts/v1/daily/1day/'
-CITY_CODE = '207697'
-API_KEY = open('api_key.txt', 'r').read()
-
-url = BASE_URL + CITY_CODE + '?apikey=' + API_KEY
-
-response = requests.get(url).json()
-
-print(response)
 
 

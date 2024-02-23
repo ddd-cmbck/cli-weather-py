198e525633e98458095fb5d8827060870bae5db7
Dim4k <dd.darknet.tt@gmail.com>
2024-02-23 18:50:08 +0000

    created class console_output_specs.py and created tests for this class (no logic inside the class for now)

---
+++ console_output_specs.py
@@ -0,0 +1,12 @@
+class ConsoleOutputSpecs:
+    def __init__(self):
+        pass
+
+    def verbose_output(self):
+        pass
+
+    def short_output(self):
+        pass
+
+    def default_output(self):
+        pass
---
+++ test_console_output_specs.py
@@ -0,0 +1,24 @@
+import unittest
+from console_output_specs import ConsoleOutputSpecs
+from unittest.mock import patch
+
+
+class TestConsoleOutputSpecs(unittest.TestCase):
+    def setUp(self):
+        """Create a ConsoleOutputSpecs instance to use in tests."""
+        self.console_output = ConsoleOutputSpecs()
+
+    def test_initializations(self):
+        pass
+
+    def test_default_output(self):
+        pass
+
+    def test_verbose_output(self):
+        pass
+
+    def test_short_output(self):
+        pass
+
+
+

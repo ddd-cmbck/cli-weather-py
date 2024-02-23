d7d47bffc3e59b66c6afae6f57c93e503fba7a70
Dim4k <dd.darknet.tt@gmail.com>
2024-02-23 23:07:57 +0000

    initialized class for weather api and tests for it

---
+++ test_weather_api_client.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import patch
+
+from weather_api_client import WeatherApiClient
+
+
+class TestWeatherApiClient(unittest.TestCase):  # to do
+
+    def setUp(self):
+        weather_api_client = WeatherApiClient()
+
+    def test_get_weather(self):
+        pass
+
---
+++ weather_api_client.py
@@ -0,0 +1,12 @@
+class WeatherApiClient:  # to do
+
+    """
+
+    Defines a common interface for all weather API clients.
+
+    """
+    def __init__(self):
+        pass
+
+    def get_weather(self, city, days, units):
+        pass
